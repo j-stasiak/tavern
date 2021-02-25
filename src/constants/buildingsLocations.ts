147295e815ba9864b2c3e3881acd2966e33a83a7
qbavv <kubawalat@gmail.com>
2021-02-25 18:43:57 +0100

    moved courses out of HUD

--- src/constants/buildingsLocations.ts
+++ src/constants/buildingsLocations.ts
@@ -2,6 +2,6 @@ export const buildings = {
   tavern: { xPosition: 0, yPosition: 200, width: 500, height: 510 },
   noobCourse: { xPosition: 725, yPosition: 300, width: 296, height: 150 },
   proCourse: { xPosition: 1150, yPosition: 300, width: 296, height: 150 },
-  adeptCourse: { xPosition: 930, yPosition: 600, width: 296, height: 150 },
-  ultraCourse: { xPosition: 1400, yPosition: 600, width: 296, height: 150 },
+  adeptCourse: { xPosition: 930, yPosition: 560, width: 296, height: 150 },
+  ultraCourse: { xPosition: 1400, yPosition: 560, width: 296, height: 150 },
 };
