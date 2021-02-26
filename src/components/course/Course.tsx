bcd3fa5cea44639417b307cf6c4e2ee1cbf8d3a7
Patryk Mysiorski <mysior@ncdc.pl>
2021-02-27 00:34:18 +0100

    style courses

--- src/components/course/Course.tsx
+++ src/components/course/Course.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from "react";
-import "./course.css";
+import React from "react";
+import "./course.scss";
 import Task from "./task/Task";
 
 const Course = ({ content, user }: any) => {
   console.log("courseuser-", user);
   return (
-    <>
+    <div className={'course'}>
       <h1>{content.title}</h1>
       <h2>{content.guide}</h2>
       {content.tasks.map((task: any) => (
         // @ts-ignore
         <Task courseId={content.id} task={task} user={user} />
       ))}
-    </>
+    </div>
   );
 };
 
