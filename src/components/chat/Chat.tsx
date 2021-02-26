3583bba78adf86dcbdbe7a7544eba92fd05406b9
Patryk Mysiorski <mysior@ncdc.pl>
2021-02-26 15:56:14 +0100

    some stupid stuff

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -67,31 +67,6 @@ const Chat = () => {
                 Send
             </button>
         </div>
-        // <div className="chat-room-container chat">
-        //     <div className="messages-container">
-        //         <ol className="messages-list">
-        //             {messages.map((message, i) => (
-        //                 <li
-        //                     key={i}
-        //                     className={`message-item ${
-        //                         message.ownedByCurrentUser ? "my-message" : "received-message"
-        //                     }`}
-        //                 >
-        //                     {message.body}
-        //                 </li>
-        //             ))}
-        //         </ol>
-        //     </div>
-        //     <textarea
-        //         value={newMessage}
-        //         onChange={handleNewMessageChange}
-        //         placeholder="Write message..."
-        //         className="new-message-input-field"
-        //     />
-        //     <button onClick={handleSendMessage} className="send-message-button">
-        //         Send
-        //     </button>
-        // </div>
     );
 };
 
