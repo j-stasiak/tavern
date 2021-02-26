bea9bea2946b1ea299e36977e97c4c4e1a0df707
Patryk Mysiorski <mysior@ncdc.pl>
2021-02-26 22:57:04 +0100

    endpoint fix

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,4 +1,4 @@
-const PLS_DONT_TOUCH_ITS_JUST_FOR_ME = "192.168.0.4";
+const PLS_DONT_TOUCH_ITS_JUST_FOR_ME = "http://192.168.0.4:";
 const SERVER_PREFIX = "http://localhost:";
 export const SERVER_URL = `${SERVER_PREFIX}3000`;
 export const SERVER_SOCKET_URL = `${SERVER_PREFIX}3001`;
