644034ce2cfa32e8000578d22076428442b96635
qbavv <kubawalat@gmail.com>
2021-02-27 01:41:43 +0100

    fix mysior blindness

--- src/components/welcomePanel/WelcomePanel.tsx
+++ src/components/welcomePanel/WelcomePanel.tsx
@@ -3,20 +3,11 @@ import RegisterPanel from "./register-panel/RegisterPanel";
 import LoginPanel from "./login-panel/LoginPanel";
 import "./WelcomePanel.scss";
 
-const WelcomePanel = ({ submitCallback }: any) => {
-  const [loginChosen, setLoginChosen] = useState(false);
-  const [registerChosen, setRegisterChosen] = useState(false);
-  if (loginChosen) {
-    return <LoginPanel submitCallback={submitCallback} />;
-  } else if (registerChosen) {
-    return <RegisterPanel submitCallback={submitCallback} />;
-  }
-  return (
-    <div className={"welcome-panel"}>
-      <LoginPanel />
-      <RegisterPanel />
-    </div>
-  );
-};
+const WelcomePanel = ({ submitCallback }: any) => (
+  <div className={"welcome-panel"}>
+    <LoginPanel submitCallback={submitCallback} />
+    <RegisterPanel submitCallback={submitCallback} />
+  </div>
+);
 
 export default WelcomePanel;
