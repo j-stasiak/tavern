2e594363c319d73b05f95499a43b79e1c01df21e
Patryk Mysiorski <mysior@ncdc.pl>
2021-02-27 10:55:24 +0100

    zeby sie nie skaszanilo

--- src/components/notes/note/Note.tsx
+++ src/components/notes/note/Note.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NoteModel } from "../../../constants/PlayerModel";
+import "./Note.scss";
+import ContentEditable from "react-contenteditable";
 
 interface Props {
   note: NoteModel;
@@ -11,18 +13,25 @@ const Note = ({ note, saveNote }: Props) => {
   useEffect(() => {
     setEditedNote(note);
   }, [note]);
+
+  const htmlForDiv = `${editedNote.description}`;
+
   return (
-    <div className={"right-side"}>
-      <textarea
-        onChange={(event) =>
-          setEditedNote({
-            ...editedNote,
-            description: event.target.value,
-          })
-        }
-        value={editedNote.description}
-      />
-      <button onClick={() => saveNote(editedNote)}>Zapisz notatkę</button>
+    <div className={"flex-col-container note-left flex-align-center"}>
+      <div className={"note-big"} onBlur={() => saveNote(editedNote)}>
+        <div className={"content"}>
+          <ContentEditable
+            html={htmlForDiv}
+            onChange={(event) =>
+              setEditedNote({
+                ...editedNote,
+                description: event.target.value,
+              })
+            }
+            className={"disable-border"}
+          />
+        </div>
+      </div>
     </div>
   );
 };
