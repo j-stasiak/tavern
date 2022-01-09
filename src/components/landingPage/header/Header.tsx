c8d96abe7468267b7f6b39d974265aa256d0f22d
Patryk Mysiorski <patryk.mysiorski@gmail.com>
2022-01-09 12:49:43 +0100

    styles refactor

--- src/components/landingPage/header/Header.tsx
+++ src/components/landingPage/header/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import { texts } from '../../../texts';
+import classNames from 'classnames';
+import flex from '../../../styles/flex.module.scss';
 
 const Header: React.FC = () => {
   const { header, description, button } = texts.landingPage;
   return (
     <header>
-      <div className={styles.overlay}>
+      <div className={classNames(styles.overlay, flex.flexRowContainer, flex.twoAxisCenter)}>
         <div className={styles.inner}>
           <h2 className={styles.title}>{header}</h2>
           <p>{description}</p>
--- src/components/navbarLinks/NavbarLinks.tsx
+++ src/components/navbarLinks/NavbarLinks.tsx
@@ -3,13 +3,14 @@ import styles from './NavbarLinks.module.scss';
 import { texts } from '../../texts';
 import classNames from 'classnames';
 import { useMenu } from '../providers/menuProvider/MenuProvider';
+import flex from '../../styles/flex.module.scss';
 
 const NavbarLinks: React.FC = () => {
   const { links } = texts.navBar;
   const { isOpen } = useMenu();
   return (
     <div className={classNames({ [styles.active]: isOpen })}>
-      <div className={classNames(styles.links)}>
+      <div className={classNames(styles.links, flex.flexRowContainer, flex.twoAxisCenter)}>
         <ul>
           {links.map(({ label, delay }) => (
             <li key={label}>
