66e1666f48d454ff3881a9d8e3f31e4475f4e505
Jakub Stasiak <jakub.stasiak@ironin.pl>
2022-01-21 22:44:36 +0100

    feat: add token to join request

--- src/phaser/SocketServer.js
+++ src/phaser/SocketServer.js
@@ -10,8 +10,11 @@ let onlinePlayers = {};
 | Colyseus connection with server
 */
 var client = new Colyseus.Client('ws://localhost:4001');
+//TODO Change 'token' to real jwt
 let room = client
-  .joinOrCreate('poke_world')
+  .joinOrCreate('poke_world', { 
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJKb2huZG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.BEtVcDngv70orUHE1xfBUMnIyBgdcdpkH0EqI_nzF80' 
+  })
   .then((room) => {
     console.log(room.sessionId, 'joined', room.name);
     return room;
