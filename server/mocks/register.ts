571d94b377f5be4b3267cfc931833ca5ac9d236f
Patryk Mysiorski <patryk.mysiorski@gmail.com>
2022-02-05 18:41:42 +0100

    add mock server

---
+++ server/mocks/register.ts
@@ -0,0 +1,10 @@
+import { RegisterResponse } from '../../src/redux/authApi/authApi';
+
+export const REGISTER_RESPONSE_MOCK: RegisterResponse = {
+  email: 'email662@email.pl',
+  username: 'mysior29',
+  firstName: null,
+  lastName: null,
+  id: 'e927fc13-9b0b-4291-ad41-f8e4d4f3ca8f',
+  isActive: false
+};
--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,8 +1,9 @@
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'http://localhost:3001'; // local
+// export const BASE_URL = 'http://localhost:4000'; // whale
 
 export const AUTH_URL = `${BASE_URL}/auth`;
-export const LOGIN_URL = '/login';
-export const REGISTER_URL = '/register';
+export const LOGIN_URL = `${AUTH_URL}/login`;
+export const REGISTER_URL = `${AUTH_URL}/register`;
 
 export const COURSE_URL = `${BASE_URL}/tutorial`;
 export const getCourseUrl = (courseId: string) => `${COURSE_URL}/${courseId}`;
